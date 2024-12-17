b6bfcee53b5b18a12b692633d0a99ba41928cfb8
Willem Wyndham <willem@ahalabs.dev>
2024-12-17 08:46:03 -0500

    feat: add thrid user and make payment

--- Stellar-dotnet-cli/AssetIssuer.cs
+++ Stellar-dotnet-cli/AssetIssuer.cs
@@ -7,21 +7,22 @@ namespace Stellar_dotnet_cli;
 
 public class AssetIssuer
 {
+    
 //Set network and server
-    public static async Task<Transaction> IssueTransaction(Account sg_issuer, Account sg_operator)
+    public static async Task<Transaction> IssueTransaction(Account sgIssuer, Account sgOperator)
     {
         //Set server
         var server = new Server("https://horizon-testnet.stellar.org");
         var clawbackEnabled = 0x8;
         var revokableEnabled = 0x2;
-        var asset = Info.Eurcv(sg_issuer);
+        var asset = Info.Eurcv(sgIssuer);
         var setOpts = new SetOptionsOperation().SetSetFlags(clawbackEnabled | revokableEnabled);
-        var changeTrust = new ChangeTrustOperation(asset, null, sg_operator.MuxedAccount);
-        var payment = new PaymentOperation(sg_operator.MuxedAccount, asset, "1");
+        var changeTrust = new ChangeTrustOperation(asset, null, sgOperator.MuxedAccount);
+        var payment = new PaymentOperation(sgOperator.MuxedAccount, asset, "10000000000");
         uint authorize = 0x1;
         uint none = 0;
-        var setTrustlineFlags = new SetTrustlineFlagsOperation(asset, sg_operator.KeyPair, none, authorize);
-        var accountInfo = await server.Accounts.Account(sg_issuer.AccountId);
+        var setTrustlineFlags = new SetTrustlineFlagsOperation(asset, sgOperator.KeyPair, none, authorize);
+        var accountInfo = await server.Accounts.Account(sgIssuer.AccountId);
         var tx = new TransactionBuilder(accountInfo)
             .SetFee(1000)
             .AddOperation(setOpts)
@@ -30,5 +31,73 @@ public class AssetIssuer
             .AddOperation(setTrustlineFlags)
             .Build();
         return tx;
+    }
+    
+    public static async Task<Transaction> TrustlineTransaction(Account sgIssuer, Account account)
+    {
+        //Set server
+        var server = new Server("https://horizon-testnet.stellar.org");
+        var asset = Info.Eurcv(sgIssuer);
+        var changeTrust = new ChangeTrustOperation(asset, null, account.MuxedAccount);
+        var accountInfo = await server.Accounts.Account(account.AccountId);
+        var tx = new TransactionBuilder(accountInfo)
+            .SetFee(1000)
+            .AddOperation(changeTrust)
+            .Build();
+        return tx;
+    }
+
+    public static async Task<Transaction> MintTransaction(Account sgIssuer, Account sgOperator, String amount)
+    {
+        var server = new Server("https://horizon-testnet.stellar.org");
+        /*
+         * # Example of minting new assets to the Operator with a sandwich transaction
+           stellar tx new set-trustline-flags --fee 1000 --build-only --source sg-issuer --asset $ASSET --trustor $OPERATOR_PK --set-authorize \
+           | stellar tx op add payment --destination $OPERATOR_PK --asset $ASSET --amount 1000000000000 \
+           | stellar tx op add set-trustline-flags --asset $ASSET --trustor $OPERATOR_PK --clear-authorize \
+           | stellar tx sign --sign-with-key sg-issuer \
+           | stellar tx send
+         */
+        var asset = Info.Eurcv(sgIssuer);
+        const uint none = 0;
+        uint authorize = 0x1;
+        var setTrustlineFlags = new SetTrustlineFlagsOperation(asset, sgOperator.KeyPair, authorize, none);
+        var clearSetTrustlineFlags =  new SetTrustlineFlagsOperation(asset, sgOperator.KeyPair,none, authorize);
+        var accountInfo = await server.Accounts.Account(sgIssuer.AccountId);
+        var payment = new PaymentOperation(sgOperator.MuxedAccount, asset, amount);
+
+        return new TransactionBuilder(accountInfo)
+            .SetFee(1000)
+            .AddOperation(setTrustlineFlags)
+            .AddOperation(payment)
+            .AddOperation(clearSetTrustlineFlags)
+            .Build();
+    }
+    
+    public static async Task<Transaction> Payment(Account sgIssuer, Account sgOperator, Account receiver, String amount)
+    {
+        var server = new Server("https://horizon-testnet.stellar.org");
+        /*
+         * # Example of minting new assets to the Operator with a sandwich transaction
+           stellar tx new set-trustline-flags --fee 1000 --build-only --source sg-issuer --asset $ASSET --trustor $OPERATOR_PK --set-authorize \
+           | stellar tx op add payment --destination $OPERATOR_PK --asset $ASSET --amount 1000000000000 \
+           | stellar tx op add set-trustline-flags --asset $ASSET --trustor $OPERATOR_PK --clear-authorize \
+           | stellar tx sign --sign-with-key sg-issuer \
+           | stellar tx send
+         */
+        var asset = Info.Eurcv(sgIssuer);
+        const uint none = 0;
+        uint authorize = 0x1;
+        var setTrustlineFlags = new SetTrustlineFlagsOperation(asset, sgOperator.KeyPair, authorize, none);
+        var clearSetTrustlineFlags =  new SetTrustlineFlagsOperation(asset, sgOperator.KeyPair,none, authorize);
+        var accountInfo = await server.Accounts.Account(sgIssuer.AccountId);
+        var payment = new PaymentOperation(receiver.MuxedAccount, asset, amount, sgOperator.MuxedAccount);
+
+        return new TransactionBuilder(accountInfo)
+            .SetFee(1000)
+            .AddOperation(setTrustlineFlags)
+            .AddOperation(payment)
+            .AddOperation(clearSetTrustlineFlags)
+            .Build();
     }
 }
--- Stellar-dotnet-cli/Program.cs
+++ Stellar-dotnet-cli/Program.cs
@@ -1,5 +1,9 @@
 ﻿using StellarDotnetSdk;
 using StellarDotnetSdk.Accounts;
+using StellarDotnetSdk.Soroban;
+//using StellarDotnetSdk.Xdr;
+using StellarDotnetSdk.Transactions;
+
 
 namespace Stellar_dotnet_cli;
 
@@ -8,18 +12,36 @@ internal class Program
     private static async Task Main(string[] args)
     {
         Network.Use(Network.Test());
+        var server = TestnetServer.Server();
         var sgIssuer = KeyPair.FromSecretSeed(args[0]);
+        var sgIssuerAccount = new Account(sgIssuer.AccountId, 1);
         var sgOperator = KeyPair.FromSecretSeed(args[1]);
-        var tx = await AssetIssuer.IssueTransaction(new Account(sgIssuer.AccountId, 1),
-            new Account(sgOperator, 1));
-        Console.WriteLine(tx.ToUnsignedEnvelopeXdrBase64());
+        var sgOperatorAccount = new Account(sgOperator.AccountId, 1);
+        var alice = KeyPair.FromSecretSeed(args[2]);
+        var aliceAccount = new Account(alice.AccountId, 1);
+
+        var tx = await AssetIssuer.IssueTransaction(sgIssuerAccount, sgOperatorAccount);
+        //Console.WriteLine(tx.ToUnsignedEnvelopeXdrBase64());
         Console.WriteLine();
         tx.Sign(sgIssuer);
         tx.Sign(sgOperator);
         Console.WriteLine(tx.ToEnvelopeXdrBase64());
-        // Console.WriteLine();
-        // Console.WriteLine(tx.ToEnvelopeXdrBase64());
-        var server = TestnetServer.Server();
+        await SubmitTransaction(tx, server);
+        
+        // Add trustline to Alice
+        var trustlineTx = await AssetIssuer.TrustlineTransaction(sgIssuerAccount, aliceAccount);
+        trustlineTx.Sign(alice);
+        await SubmitTransaction(trustlineTx, server);
+        
+        var paymentTx = await AssetIssuer.Payment(sgIssuerAccount, sgOperatorAccount, aliceAccount, "10000000");
+        paymentTx.Sign(sgIssuer);
+        paymentTx.Sign(sgOperator);
+        await SubmitTransaction(paymentTx, server);
+        
+    }
+
+    private static async Task SubmitTransaction(Transaction tx, SorobanServer server)
+    {
         var result = await server.SendTransaction(tx);
         Console.WriteLine(result.Status);
         var res = await TestnetServer.GetTransaction(result.Hash, server);
